19c07eebf38f2e58c10fb84982088be7d81fae9b
Diego <65690176+dmoprates@users.noreply.github.com>
2021-01-29 06:57:04 -0300

    Arquivos das aulas
    Commit

--- projeto/src/servidor.js
+++ projeto/src/servidor.js
@@ -2,9 +2,25 @@ const porta = 3003
 
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
+const bancoDeDados = require('./bancoDeDados')
+
+app.use(bodyParser.urlencoded({extended: true}))
 
 app.get('/produtos', (req, res, next) => {
-    res.send({nome: 'Notebook', preco: 123.45}) // Converter para JSON
+    res.send(bancoDeDados.getProdutos())
+})
+
+app.get('/produtos/:id', (req, res, next) => {
+    res.send(bancoDeDados.getProduto(req.params.id))
+})
+
+app.post('/produtos', (req, res, next) => {
+    const produto = bancoDeDados.salvarProduto({
+        nome: res.body.nome, 
+        preco: req.body.preco
+    })
+    res.send(produto)
 })
 
 app.listen(porta, () => {
